83b31cccce509a43e7b7ba20a9c4c3d8cc7830a9
hannah <hannah.l.curran@gmail.com>
2023-08-02 12:14:55 +0100

    add edit delete finch

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.urls import reverse 
 
 # Create your models here.
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     age = models.IntegerField()
-    habitat = models.TextField(max_length=150)
+    habitat = models.TextField(max_length=600)
    
     def __str__(self):
         return self.name 
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Finch
 
 # Create your views here.
@@ -18,3 +20,16 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    success_url='/finches/{finch_id}'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields= ['age', 'breed', 'habitat']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url= '/finches'
