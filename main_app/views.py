639a2f647eaf995959b19e80c9e0c4004cf8a975
hannah <hannah.l.curran@gmail.com>
2023-08-03 17:21:35 +0100

    add feedings for finch

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Finch
+
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request): 
@@ -19,7 +21,20 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', { 'finch': finch })
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the finch_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id) 
 
 class FinchCreate(CreateView):
     model = Finch
